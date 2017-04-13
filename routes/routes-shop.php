e4b85be69e08e5d66eed995d6d90a65c8f2096de
Clemsouille <clement.rosenberger@viacesi.fr>
2017-04-13 17:50:46 +0200

    create routes shop

--- routes/routes-shop.php
+++ routes/routes-shop.php
@@ -6,6 +6,39 @@
  * Time: 15:41
  */
 
-Route::get('/shop', function () {
-    return view('welcome shop');
+
+
+Route::group(['prefix' => 'shop'], function(){
+
+    //accueil boutique
+    Route::get('/', ['as' => 'shop_home' ,function () {
+        return "Boutique accueil";
+    }]);
+
+
+    //Groupe avec middleware : interdit aux noms membres du cesi
+    Route::group(['middleware' => 'role.cesi'], function(){
+
+        //Ajout d'un produit -> CESI
+        Route::get('/add-product', ['as' => 'shop_add_product', function(){
+            return "add a product";
+        }]);
+
+    });
+
+
+    //Panier d'un produit
+    Route::get('/basket', ['as' => 'shop_basket', function(){
+        return "panier";
+    }]);
+
+    //Tous les produits d'une catégorie
+    Route::get('/{category}', ['as' => 'shop_products_categories', function($category){
+        return "Catégorie : $category";
+    }])->where('category', '[a-z0-9\-]+');
+
+    //Présentation d'un produit
+    Route::get('/{category}/{product}', ['as' => 'shop_product', function($category, $product){
+        return "Catégorie : $category '\n' Product : $product";
+    }])->where('category', '[a-z0-9\-]+')->where('product', '[a-z0-9\-]+');
 });
