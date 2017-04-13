cc5853ac57d799e13dfd487b82033ac599ec6b81
Clemsouille <clement.rosenberger@viacesi.fr>
2017-04-13 16:04:36 +0200

    add route shop

---
+++ routes/routes-boutiques.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Clemsouille
+ * Date: 13/04/2017
+ * Time: 15:41
+ */
+
+Route::get('/shop', function () {
+    return view('welcome shop');
+});
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,5 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+require_once('routes-boutiques.php');
