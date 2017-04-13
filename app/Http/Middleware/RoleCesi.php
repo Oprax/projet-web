42e71c85afd2b528ce4491d4cde01233670990d0
Clemsouille <clement.rosenberger@viacesi.fr>
2017-04-13 17:44:48 +0200

    Create middleware "role cesi"

---
+++ app/Http/Middleware/RoleCesi.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class RoleCesi
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    
+    //si l'utisateur à le role de CESI il peut continuer
+    public function handle($request, Closure $next)
+    {
+        $user = $request->user();
+
+        if ($user && $user->isCesi())
+        {
+            return $next($request);
+        }
+        return new RedirectResponse(url('/'));
+    }
+}
