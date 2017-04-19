0c523e65e215a099d116cd42dcfbebf008c2db39
Ankanoid <jeansebbah@outlook.fr>
2017-04-19 14:27:05 +0200

    css + calendar

--- public/js/style.js
+++ public/js/style.js
@@ -2,9 +2,10 @@
  * Created by Clemsouille on 19/04/2017.
  */
 $(document).ready(function () {
-    $('#addimage').click(function() {
-       
+    $('#MenuButtonSideBar').click(function() {
+        $(".fc-day-grid-container").css("height", "240");
     });
-
-
 });
+
+
+
