34285b3ee7aff190f3b6f1f3b645b847d4a20685
Clemsouille <clement.rosenberger@viacesi.fr>
2017-04-20 15:51:13 +0200

    add must

--- resources/views/pages/shop/index.blade.php
+++ resources/views/pages/shop/index.blade.php
@@ -12,8 +12,8 @@
                 @if($product->category->name == $category->name AND $number < 5)
                     @php($number++)
                 <div class="card">
-                    <div id="extra" class="extra">
-                        <a class="header" href="{{ route('shop_product', ['category' => $category->name, 'product' => $product->slug]) }}" ><h5>{{ str_limit($product->name, 15) }}</h5></a>
+                    <div id="extra" class="extra" style="padding-top: 0; padding-bottom: 0; margin-top: 0; ">
+                        <a class="header" href="{{ route('shop_product', ['category' => $category->name, 'product' => $product->slug]) }}" ><h4>{{ str_limit($product->name, 15) }}</h4></a>
                     </div>
 <!--Afficher premiere image dans index -->
                     <div class="image">
@@ -23,8 +23,8 @@
                             {{ Html::image('images/shop/image_default.png') }}
                         @endif
                     </div>
-                    <div id="extra1" class="extra">
-                        {{ $product->price }} €
+                    <div id="extra1" class="extra" style="padding-top: 0; padding-bottom: 0; margin-top: 0; margin-bottom: 0;">
+                        <h5>{{ $product->price }} €</h5>
                     </div>
                 </div>
                 @endif
