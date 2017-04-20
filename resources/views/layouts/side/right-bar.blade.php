536b123c09cd50e2d2d16291f6ba39dfdf584351
Unknown <arno.birchler@viacesi.fr>
2017-04-20 17:14:24 +0200

    Fix href on products right bar

--- resources/views/layouts/side/right-bar.blade.php
+++ resources/views/layouts/side/right-bar.blade.php
@@ -7,21 +7,22 @@
         @foreach ($Products as $product)
         <div id="productside" class="ui segment">
             <div class="ui centered aligned header">
-                {{ $product->name }}
+                <a href="{{route('shop_product', [$product->category_id, $product->slug])}}">{{ $product->name }}</a>
             </div>
             <div class="ui grid">
-                <img class="ui  centered medium image" src="@if(isset($product->pictures[0]))
+                <a href="{{route('shop_product', [$product->category_id, $product->slug])}}"><img class="ui  centered medium image" src="@if(isset($product->pictures[0]))
                 {{ asset($product->pictures[0]->url) }}
                 @else
                 {{'images/shop/image_default.png' }}
-                @endif">
+                @endif"></a>
             </div>
             <div class="ui center aligned grid">
                 <div id="buyproduct" class="ui circular segment">
+                    <a href="{{route('shop_product', [$product->category_id, $product->slug])}}">
                     <h4 class="ui header">
                         Acheter maintenant !
                         <div class="sub header">{{$product->price}}€</div>
-                    </h4>
+                    </h4></a>
                 </div>
             </div>
         </div>
