5af2f2293874ec311d0f6beacbe0433906888070
Oprax <oprax@me.com>
2017-04-20 17:37:04 +0200

    Update Behat scenario

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -15,4 +15,14 @@ class FeatureContext extends MinkContext
         $this->getSession()->wait($arg1 * 1000);
     }
 
+    /**
+     * @Given I click on :arg1
+     */
+    public function iClickOn($arg1)
+    {
+        $page = $this->getSession()->getPage();
+        $el = $page->find('named', ['content', $arg1]);
+        $el->click();
+    }
+
 }
